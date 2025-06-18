3d1ea3083f72eea4d8a6f6d2c390ad239c80cd3d
DevEmmy <eolaosebikan60@gmail.com>
2025-06-19 00:05:17 +0100

    fix: add optional chaining in capitalizeResourceName method to handle potential undefined values

--- src/api/v1/controllers/controller.ts
+++ src/api/v1/controllers/controller.ts
@@ -177,7 +177,8 @@ export default abstract class Controller<T> {
       .split(' ')
       .map(word => {
         if (!word) return '';
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
+        return word.charAt(0).toUpperCase() + word?.slice(1)?.toLowerCase();
       })
       .join(' ');
   }
