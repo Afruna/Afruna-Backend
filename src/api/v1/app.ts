f56ac464216599173b9360cea2b244947521ae34
Emmanuel Olaosebikan <eolaosebikan60@gmail.com>
2025-07-07 15:00:42 +0100

    Update app.ts

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -155,22 +155,22 @@ class App {
 
   private initMiddlewares() {
     this.app.set('trust proxy', 1);
-    // this.app.use(
-    //   cors({
-    //     origin: ['http://localhost:3000', 'https://www.afruna.com', 'https://afruna.com', 'http://localhost:3001', 'https://vendor-service-portal.vercel.app', 'https://vendor.afruna.com'
-    //       , 'https://auth.afruna.com', "https://admin.afruna.com",
-    //     ],
-    //     credentials: true,
-    //     exposedHeaders: ['set-cookie'],
-    //   }),
-    // );
     this.app.use(
       cors({
-        origin: "*",
+        origin: ['http://localhost:3000', 'https://www.afruna.com', 'https://afruna.com', 'http://localhost:3001', 'https://vendor-service-portal.vercel.app', 'https://vendor.afruna.com'
+          , 'https://auth.afruna.com', "https://admin.afruna.com", 'https://staging.afruna.com', 'https://staging.vendor.afruna.com', 'https://staging.admin.afruna.com'
+        ],
         credentials: true,
         exposedHeaders: ['set-cookie'],
       }),
     );
+    // this.app.use(
+    //   cors({
+    //     origin: "*",
+    //     credentials: true,
+    //     exposedHeaders: ['set-cookie'],
+    //   }),
+    // );
 
     // Initialize session middleware before passport
     //this.initCookieSession(Config.DB_URI);
