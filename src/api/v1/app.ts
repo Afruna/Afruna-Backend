3ae61f5021c9169c4cdb1734fcd4adf0a11f7a57
Emmanuel Olaosebikan <eolaosebikan60@gmail.com>
2025-07-07 14:19:51 +0100

    Update app.ts

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -155,7 +155,7 @@ class App {
 
   private initMiddlewares() {
     this.app.set('trust proxy', 1);
-    this.app.use(
+    // this.app.use(
     //   cors({
     //     origin: ['http://localhost:3000', 'https://www.afruna.com', 'https://afruna.com', 'http://localhost:3001', 'https://vendor-service-portal.vercel.app', 'https://vendor.afruna.com'
     //       , 'https://auth.afruna.com', "https://admin.afruna.com",
